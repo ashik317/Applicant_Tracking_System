75ee3071754117c76887577f82b76ea5fd5f52d7
ashik317 <mdarlikhon317@gmail.com>
2025-12-18 15:14:45 +0600

    Ignore entrypoint.sh

--- entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e
-python manage.py collectstatic --noinput || true
-python manage.py migrate --noinput
-gunicorn Applicant_Tracking_System.wsgi:application -b 0.0.0.0:8000 --workers 3
